ef5dadb1514fd857febd499ebd035e53815aaaed
Luke Poirrier <lukepoirrier5@gmail.com>
2021-07-20 14:52:35 -0400

    Fix bugs, fix style, added new color scheme

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,7 +89,7 @@ function runTimer() {
         secondsLeft--;
         timeEl.textContent = secondsLeft - timeToSubtract;
         // timeToSubtract = 0;
-        if(secondsLeft - timeToSubtract <= 0){
+        if (secondsLeft - timeToSubtract <= 0) {
             clearInterval(timerInterval);
             endGame();
         }
@@ -98,21 +98,26 @@ function runTimer() {
 }
 
 function startGame() {
-        setQuestion();
-        timeToSubtract = 0;
-        runTimer();
-        document.querySelector(".answerArea").addEventListener("click", answerClick);
+    setQuestion();
+    timeToSubtract = 0;
+    runTimer();
+    document.querySelector(".answerArea").addEventListener("click", answerClick);
     //currentQuestion = -1;
 
 }
 function setQuestion() {
+    // Stop when the end of the questions object has been reached  
     if (currentQuestion > questions.length) {
         clearInterval(timerInterval);
         endingScore = timerInterval.secondsLeft;
         return;
-    } else if (currentQuestion < questions.length -1 ) {
+        // Continue if we are still within the length of the questions object
+
+    } else if (currentQuestion < questions.length - 1) {
         currentQuestion++;
+        // Set the question from the questions object using the currentQuestion var
         questionEl.textContent = questions[currentQuestion].question;
+        // Set the answer selection from questions[i].answers for the current question
         aSpan.textContent = questions[currentQuestion].answers["a"];
         bSpan.textContent = questions[currentQuestion].answers["b"];
         cSpan.textContent = questions[currentQuestion].answers["c"];
@@ -126,10 +131,17 @@ function setQuestion() {
 // Event Listener for the answers when clicked
 answerEl.addEventListener("click", answerClick)
 
+// Function to be run when an answer is clicked during the quiz
 function answerClick(event) {
+    // Get the target in the element variable
     var element = event.target;
+    // Grab the correct answer from the questions object
     var correctAnswer = questions[currentQuestion].correctAnswer;
+    // Get the contents of the answer the user clicked
     var selectedAnswer = element.textContent;
+
+    /* Work around to reduce time left/ score when the last question is reached
+        Before, it wouldn't take away time if the user got the last question wrong*/
     if (currentQuestion >= questions.length - 1) {
         answerEl.removeEventListener("click", answerClick);
         if (element.matches("h3.answer") || element.matches("span")) {
@@ -139,15 +151,17 @@ function answerClick(event) {
                 timeToSubtract = timeToSubtract + 5;
                 console.log("Incorrect!")
             }
-        }else{
+        } else {
             return;
         }
+        // End game after evaluating the final question
         endGame();
         // return;
     } else {
-        // var element = event.target;
-        // var correctAnswer = questions[currentQuestion].correctAnswer;
-        // var selectedAnswer = element.textContent;
+        /* Evaluate the accuracy of the answer chosen by the user by:
+             - Making sure an answer element was clicked
+             - Checking to see if it matches the correctAnswer for the currentQuestion
+             - If it's incorrect, subtract 5 seconds from the time left/score */
         if (element.matches("h3.answer") || element.matches("span")) {
             if (selectedAnswer == questions[currentQuestion].answers[correctAnswer]) {
                 console.log("correct!")
@@ -155,97 +169,101 @@ function answerClick(event) {
                 timeToSubtract = timeToSubtract + 5;
                 console.log("Incorrect!")
             }
-        }else{
+        } else {
             return;
         }
-        console.log(currentQuestion);
-        // currentQuestion++;
-
+        // Set up the next question and increment currentQuestion
         setQuestion();
     }
 }
-//Start button event listener
 
-
-function hideStart(){
+/* These are function to show and hide the various buttons of the game.
+    I followed the rule of abstracting anything you're going to do more 
+    than once out into a function. Saved a lot of time. */
+function hideStart() {
     startButton.setAttribute("style", "display: none;");
 }
-function hideHighscoreButton(){
-    viewHighscores.setAttribute("style","display:none");
+function hideHighscoreButton() {
+    viewHighscores.setAttribute("style", "display:none");
 }
-function hideMenuButton(){
-    mainMenu.setAttribute("style","display:none");
+function hideMenuButton() {
+    mainMenu.setAttribute("style", "display:none");
 }
-function hideClearButton(){
-    clearButton.setAttribute("style","display:none");
+function hideClearButton() {
+    clearButton.setAttribute("style", "display:none");
 }
-function hideSubmitButton(){
-    submitButton.setAttribute("style","display:none;");
+function hideSubmitButton() {
+    submitButton.setAttribute("style", "display:none;");
 }
-function showStart(){
+function showStart() {
     startButton.setAttribute("style", "display: block;");
 }
-function showHighscoreButton(){
-    viewHighscores.setAttribute("style","display:block;");
+function showHighscoreButton() {
+    viewHighscores.setAttribute("style", "display:block;");
 }
-function showMenuButton(){
-    mainMenu.setAttribute("style","display:inline;");
+function showMenuButton() {
+    mainMenu.setAttribute("style", "display:inline;");
 }
-function showClearButton(){
-    clearButton.setAttribute("style","display:inline;");
+function showClearButton() {
+    clearButton.setAttribute("style", "display:inline;");
 }
-function showSubmitButton(){
-    submitButton.setAttribute("style","display:block;");
+function showSubmitButton() {
+    submitButton.setAttribute("style", "display:block;");
 }
-function hideHighscoreInput(){
+function hideHighscoreInput() {
     highscoreForm.setAttribute("style", "display:none;")
 }
-function showHighscores(){
+function showHighscores() {
     newScore.setAttribute("style", "font-size: 2rem; color: white;");
     newScore.className += " show ";
     highscoreEl.className += " highscore-container show highscoreEl";
-    // highscoreEl.classList.add("show highscoreEL");
     scoreLabel.className += " show ";
     clearButton.classList.add("show-inline");
     mainMenu.classList.add("show-inline");
 }
 
 function displayHighscore() {
-    if(scoresGenerated == false){
-    questionEl.textContent = "";
-    console.log(retrieveHighscore());
-    highscoreEl.setAttribute("style", "display: block;")
-    highscoreList.setAttribute("display", "contents");
-    // ? Variable declaration for generated Elements in Highscores
-    // ? Setting Text content of generated score frame
-    scoreHeader.textContent = "Highscores";
-    scoreHeader.setAttribute("class", "scoreHeader");
-    scoreLabel.textContent = "Your Score:";
-    newScore.textContent = timeEl.innerHTML;
-    // window.newScore = timeEl.innerHTML;
-    newScore.setAttribute("class", "newScore");    
-    highscoreEl.setAttribute("style", "")
-    highscoreEl.setAttribute("class", "highscoreEL");
-    scoreLabel.setAttribute("class", "scoreLabel");
-    clearButton.setAttribute("style", "display: inline;");
-    mainMenu.setAttribute("style", "display:inline;")
-    // ? Appending score context to the highscore container    
-    highscoreTitle.append(scoreHeader);
-    highscoreEl.appendChild(scoreLabel);
-    highscoreEl.appendChild(newScore);
-    scoresGenerated = true;
-    }else{
+    // Check to see if the highscores have already been generated, if not, generate them
+    if (scoresGenerated == false) {
+        // Clear question from .questionArea
+        questionEl.textContent = "";
+        console.log(retrieveHighscore());
+        // Set display properties of the highscore container and list of highscores
+
+        highscoreEl.setAttribute("style", "display: block;")
+        highscoreList.setAttribute("display", "contents");
+        // Setting Text content of generated score frame
+        scoreHeader.textContent = "Highscores";
+        scoreLabel.textContent = "Your Score:";
+        newScore.textContent = timeEl.innerHTML;
+        // Reset style of highscore container to start with a clean slate
+        highscoreEl.setAttribute("style", "")
+    // Set classes for dynamically generated highscores page
+        newScore.setAttribute("class", "newScore");
+        scoreHeader.setAttribute("class", "scoreHeader");
+        highscoreEl.setAttribute("class", "highscoreEL");
+        scoreLabel.setAttribute("class", "scoreLabel");
+        // Show Clear and Main Menu Buttons
+        clearButton.setAttribute("style", "display: inline;");
+        mainMenu.setAttribute("style", "display:inline;")
+        // Appending score context to the highscore container    
+        highscoreTitle.append(scoreHeader);
+        highscoreEl.appendChild(scoreLabel);
+        highscoreEl.appendChild(newScore);
+        scoresGenerated = true;
+    } else {
+        // If scores are already generated, display them
         showHighscores();
     }
 }
 
-function hideHighscores(){
+function hideHighscores() {
     highscoreEl.classList.remove("show");
     highscoreEl.classList.add("hidden");
-    clearButton.setAttribute("style","display:none;")
-    mainMenu.setAttribute("style","display:none;")
+    clearButton.setAttribute("style", "display:none;")
+    mainMenu.setAttribute("style", "display:none;")
 }
-// ? Submit, Clear, and Main Menu Event handlers
+//  Submit, Clear, and Main Menu Event handlers
 
 startButton.addEventListener("click", function (event) {
     startGame();
@@ -254,7 +272,7 @@ startButton.addEventListener("click", function (event) {
     hideHighscoreButton();
 })
 
-viewHighscores.addEventListener("click", function(){
+viewHighscores.addEventListener("click", function () {
     displayHighscore();
     showMenuButton();
     showClearButton();
@@ -272,15 +290,14 @@ submitButton.addEventListener("click", function (event) {
     hideHighscoreInput();
 })
 
-clearButton.addEventListener("click", function(){
+clearButton.addEventListener("click", function () {
     localStorage.clear("highscores");
     retrieveHighscore();
     removeAllChildNodes(highscoreList);
-    // displayHighscore();
 })
 
-mainMenu.addEventListener("click", function(){
-    startButton.setAttribute("style","display:block;");
+mainMenu.addEventListener("click", function () {
+    startButton.setAttribute("style", "display:block;");
     showHighscoreButton();
     hideHighscoreInput();
     hideSubmitButton();
@@ -304,7 +321,7 @@ function storeHighscore(initials, score) {
         highscores[initials] = score;
         console.log(highscores);
         localStorage.setItem("highscores", JSON.stringify(highscores));
-    } 
+    }
     highscores[initials] = score;
     console.log(highscores);
     localStorage.setItem("highscores", JSON.stringify(highscores));
@@ -326,7 +343,7 @@ function retrieveHighscore() {
             scoreX.textContent = key + ": " + value;
             highscoreList.append(scoreX);
         }
-        // return parsedScore;
+        return parsedScore;
     }
 }
 function getInitials() {
@@ -340,14 +357,11 @@ function endGame() {
     questionEl.textContent = '';
     timeEl.textContent = secondsLeft - timeToSubtract;
     newScore.textContent = timeEl.textContent;
-    // answerEl.children().textContent = '';
     document.querySelector(".answerArea").removeEventListener("click", answerClick);
-    // score = secondsLeft;
-    answerEl.setAttribute("style","display:none;")
+    answerEl.setAttribute("style", "display:none;")
     showSubmitButton();
     getInitials();
     currentQuestion = -1;
-    // console.log(score);
     var alreadyRun = false;
     if (alreadyRun == false) {
         alreadyRun = true;
